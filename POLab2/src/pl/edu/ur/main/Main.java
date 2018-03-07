11ea9703307a22dc8904c34cded6a2cedc9dcc79
student <student@pc-108-22>
2018-03-07 17:54:06 +0100

    Zadania Lab

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marcin
@@ -10,7 +12,76 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int wybor = 1;
+        while (wybor != 0) {
+            wybor = input.nextInt();
+            switch (wybor) {
+                case 1:
+                    System.out.println("Styczen");
+                    break;
+                case 2:
+                    System.out.println("Luty");
+                    break;
+                case 3:
+                    System.out.println("Marzec");
+                    break;
+                case 4:
+                    System.out.println("Kwiecien");
+                    break;
+                case 5:
+                    System.out.println("Maj");
+                    break;
+                case 6:
+                    System.out.println("Czerwiec");
+                    break;
+                case 7:
+                    System.out.println("Lipiec");
+                    break;
+                case 8:
+                    System.out.println("Sierpien");
+                    break;
+                case 9:
+                    System.out.println("Wrzesien");
+                    break;
+                case 10:
+                    System.out.println("Pazdziernik");
+                    break;
+                case 11:
+                    System.out.println("Listopad");
+                    break;
+                case 12:
+                    System.out.println("Grudzien");
+                    break;
+                default:
+                    System.out.println("Bledny numer");
+                    break;
+            }
+        }
+        int liczba1 = 10, liczba3 =0,i=0;
+
+        while(liczba3 < 1000)
+        {
+            liczba3 += liczba1;
+            i++;
+        }
+        System.out.println(i);
+        liczba3 =0;
+        i=0;
+        do{
+            liczba3 += liczba1;
+            i++;
+        }while(liczba3 < 1000);        
+        System.out.println(i);
+        
+        for (int j = 0; j <= 20; j++) {
+            if(j==2 || j==6 || j==9 || j==15 || j==19){
+                continue;
+        }
+            System.out.println(j);
+        }  
         // TODO zad 3, 4, 6, 7
-    }
     
+    }
 }
